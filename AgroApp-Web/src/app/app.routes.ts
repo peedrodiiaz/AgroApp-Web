3705075937cc2e157fb32686dddde721da59ca83
Pedro <diaz.gaped24@triana.salesianos.edu>
2025-11-05 14:25:22 +0100

    register html finalizado

--- AgroApp-Web/src/app/app.routes.ts
+++ AgroApp-Web/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login.component/login.component';
+import { RegisterComponent } from './pages/register.component/register.component';
 
 export const routes: Routes = [
     {
         path: 'inicio-sesion',
         component: LoginComponent,
+    },
+    {
+        path: 'registro',
+        component: RegisterComponent
+
     }
 
 ];
