9cef54e713686581b97edecf6b6fd8367a0ff6bd
Pedro <diaz.gaped24@triana.salesianos.edu>
2025-11-05 10:00:38 +0100

    queda arreglar para ponerlo más bonito el inicio de sesion

---
+++ AgroApp-Web/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login.component/login.component';
+
+export const routes: Routes = [
+    {
+        path: 'inicio-sesion',
+        component: LoginComponent,
+    }
+
+];
