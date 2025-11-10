024a73d3b4c75aebbb1c8f77d1468ba49dc1a992
Pedro <diazpd2006>
2025-11-10 20:04:06 +0100

    subida

--- AgroApp-Web/src/app/app.routes.ts
+++ AgroApp-Web/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login.page/login.pages';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout/admin-layout';
+import { DashboardComponent } from './pages/dashboard/dashboard/dashboard';
 
 
 export const routes: Routes = [
@@ -7,6 +9,15 @@ export const routes: Routes = [
         path: 'inicio-sesion',
         component: LoginComponent,
     },
-    
+    {
+    path: '',
+    component: AdminLayoutComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      // Aquí puedes añadir más páginas hijas (máquinas, incidencias...)
+    ]
+  },
+
+
 
 ];
--- AgroApp-Web/src/app/layouts/admin-layout/admin-layout/admin-layout.ts
+++ AgroApp-Web/src/app/layouts/admin-layout/admin-layout/admin-layout.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from '../sidebar/sidebar';
 import { TopbarComponent } from '../topbar/topbar';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [CommonModule, SidebarComponent, TopbarComponent],
+  imports: [CommonModule, SidebarComponent, TopbarComponent, RouterOutlet],
   templateUrl: './admin-layout.html',
   styleUrls: ['./admin-layout.css'],
 })
