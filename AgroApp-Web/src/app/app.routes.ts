ac668bce37ea5c85b0176539c7430b394b9cf703
Pedro <diaz.gaped24@triana.salesianos.edu>
2025-11-12 09:09:12 +0100

    subida

--- AgroApp-Web/src/app/app.routes.ts
+++ AgroApp-Web/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login.page/login.pages';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout/admin-layout';
 import { DashboardComponent } from './pages/dashboard/dashboard/dashboard';
-import { MaquinasComponent } from './pages/maquinas/maquinas';
+import { MaquinasComponent } from './pages/maquinas/activas/maquinas';
 
 
 
@@ -11,20 +11,13 @@ export const routes: Routes = [
         path: 'inicio-sesion',
         component: LoginComponent,
     },
-    {
-    path: '',
-    component: AdminLayoutComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      // Aquí puedes añadir más páginas hijas (máquinas, incidencias...)
-    ]
-  },
+    
   {
     path: '',
     component: AdminLayoutComponent,
     children: [
-      { path: '', component: DashboardComponent },
-      { path: 'maquinas', component: MaquinasComponent }, // Nueva ruta
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'maquinas', component: MaquinasComponent },
     ]
   },
 
--- AgroApp-Web/src/app/pages/login.page/login.pages.ts
+++ AgroApp-Web/src/app/pages/login.page/login.pages.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -12,10 +12,22 @@ import { CommonModule } from '@angular/common';
 export class LoginComponent {
 
   loginForm = new FormGroup({
-    usuario: new FormControl(''),
-    password: new FormControl(''),
+    usuario: new FormControl('', {
+      validators: [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.maxLength(50),
+        Validators.pattern('^[A-Za-zÁÉÍÓÚáéíóúÑñüÜ\\s]+$')
+      ]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, 
+        Validators.minLength(8), 
+        Validators.maxLength(50)],
+    }),
     rememberMe: new FormControl(false),
   });
+  get f() {return this.loginForm.controls;}
 
   onSubmit() {
     console.log('** Values **');
