2433e24e6797f43e5d1a3822966117c7fb5d9464
Pedro <diaz.gaped24@triana.salesianos.edu>
2026-01-14 09:00:59 +0100

    implementacion roles

--- AgroApp-Web/src/app/services/auth.service.ts
+++ AgroApp-Web/src/app/services/auth.service.ts
@@ -10,58 +10,44 @@ export class AuthService {
   private http = inject(HttpClient);
   private apiUrl = 'http://localhost:8000/api';
 
-  /**
-   * Registrar un nuevo usuario
-   */
+
   register(data: any): Observable<RegistroResponse> {
     return this.http.post<RegistroResponse>(`${this.apiUrl}/register`, data);
   }
 
-  /**
-   * Iniciar sesión
-   */
+
   login(credentials: any): Observable<LoginResponse> {
+  
     return this.http.post<LoginResponse>(`${this.apiUrl}/login`, credentials);
   }
 
-  /**
-   * Cerrar sesión
-   */
+
   logout(): Observable<any> {
     return this.http.post(`${this.apiUrl}/logout`, {});
   }
 
-  /**
-   * Obtener información del usuario autenticado
-   */
+
   getUser(): Observable<Usuario> {
     return this.http.get<Usuario>(`${this.apiUrl}/user`);
   }
 
-  /**
-   * Verificar si el usuario está autenticado
-   */
+
   isAuthenticated(): boolean {
     return !!this.getToken();
   }
+  
 
-  /**
-   * Obtener el token del localStorage
-   */
+
   getToken(): string | null {
     return localStorage.getItem('token');
   }
 
-  /**
-   * Guardar el token en el localStorage
-   */
+
   saveToken(token: string): void {
     localStorage.setItem('token', token);
   }
 
-  /**
-   * Eliminar el token del localStorage
-   */
+
   removeToken(): void {
     localStorage.removeItem('token');
   }
